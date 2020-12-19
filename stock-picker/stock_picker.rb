fc803af17f48094390a405323b8d4068f5e50cbb
Jake Dam <robert.dam2@gmail.com>
2020-12-19 18:40:01 -0500

    finished stock picker function

--- stock-picker/stock_picker.rb
+++ stock-picker/stock_picker.rb
@@ -1,7 +1,19 @@
 #Stock picker function
 def stock_picker(array)
-    results = []
-    array.each_with_index |price, index| do
-
+  results = []
+  profit = 0
+  array.each_with_index do |buy_price, buy_day|
+    array.each_with_index do |sell_price, sell_day|
+      if sell_day > buy_day
+        if (sell_price - buy_price) > profit
+          profit = sell_price - buy_price
+          results[0] = buy_day
+          results[1] = sell_day
+        end
+      end
+    end
+  end
+  results
+  puts "Buy on day #{results[0]} and sell on day #{results[1]} for a max profit of $#{profit}"
 end 
 stock_picker([17,3,6,9,15,8,6,1,10])
